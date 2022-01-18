254e1cc161d61e2a71e9afaffe130a6f9664829f
WinnieCWu <winniechn7@gmail.com>
2022-01-18 15:39:53 -0800

    handled form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 //console.log(buttonEl); --> replaced with the var expressions below
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //need to place createTaskHandler before eventListener or else undef
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //prevents refresh from happening
+    event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
