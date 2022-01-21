fad7d2bff5758bd1c873c0ac153de10ade75a30e
WinnieCWu <winniechn7@gmail.com>
2022-01-20 22:31:45 -0800

    capture input values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
 var formEl = document.querySelector("#task-form");
-//console.log(buttonEl); --> replaced with the var expressions below
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//need to place createTaskHandler before eventListener or else undef
-var createTaskHandler = function(event) {
-    //prevents refresh from happening
-    event.preventDefault();
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
-
-
